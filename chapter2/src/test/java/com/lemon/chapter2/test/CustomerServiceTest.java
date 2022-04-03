d6bbce89fb67600552aee340bef417d837927d2a
lemonliu1993 <2305247091@qq.com>
2022-04-03 22:59:07 +0800

    junit

--- chapter2/src/test/java/com/lemon/chapter2/test/CustomerServiceTest.java
+++ chapter2/src/test/java/com/lemon/chapter2/test/CustomerServiceTest.java
@@ -1,8 +1,14 @@
 package com.lemon.chapter2.test;
 
+import com.lemon.chapter2.model.Customer;
 import com.lemon.chapter2.service.CustomerService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lemoon on 2022/4/2 11:32 PM
@@ -16,8 +22,48 @@ public class CustomerServiceTest {
     }
 
     @Before
-    public void init(){
+    public void init() {
         //TODO 初始化数据库
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception {
+        List<Customer> customerList = customerService.getCustomerList("");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() throws Exception {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone", "13023111344");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
     }
 
 
